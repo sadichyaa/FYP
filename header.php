34219abae85e65fbae2fa072fda16a3e0dec19c9
sadichyaa <sadikshyanepal454@gmail.com>
2025-11-27 20:43:59 +0545

    edited the employeelogin.php, index.phpheader.php

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Auction</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .navbar {
+            background: #333;
+            padding: 10px;
+        }
+        .navbar a {
+            color: white;
+            margin-right: 15px;
+            text-decoration: none;
+            font-size: 18px;
+        }
+        .navbar a:hover {
+            color: yellow;
+        }
+    </style>
+</head>
+<body>
+
+<div class="navbar">
+    <a href="index.php">Home</a>
+    <a href="registry.php">Register</a>
+    <a href="employeelogin.php">Employee Login</a>
+</div>
+
+<hr>
